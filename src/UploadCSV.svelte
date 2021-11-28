65e76def53b9f428037a021bf04b298f0aca3f0f
Harish Chander <admin@infobug.org>
2021-11-28 08:41:36 +0400

    Patch

--- src/UploadCSV.svelte
+++ src/UploadCSV.svelte
@@ -1,58 +1,74 @@
 <script>
-    import PapaParse from 'papaparse'
+  import PapaParse from 'papaparse';
 
-	export let onUpload;
-	
-	export let dataToCSV = [];
-	let prevDataToCSV;
+  export let onUpload;
 
-	export let allowedFileExtensions = ['csv'];
-	
-	let maxFileSize = 3145728;
+  export let dataToCSV = [];
+  let prevDataToCSV;
 
-	// this is the variable that the file gets bound to
-	let uploader;
+  export let allowedFileExtensions = ['csv'];
 
-	$: {
-		// called on props change
-		if (dataToCSV !== prevDataToCSV && dataToCSV.length !== 0) {
-			prevDataToCSV = dataToCSV;
-			const csvData = PapaParse.unparse(dataToCSV);
-			onUpload ? onUpload(csvData) : console.log("Remember to define an onUpload function as props. CSV Data:", csvData);
-		}
-	}
+  let maxFileSize = 3145728;
 
-	function uploadFile(event) {
-		event.preventDefault();
-		const file = uploader.files[0];
+  // this is the variable that the file gets bound to
+  let uploader;
 
-		const fileExtensionArray = file.type.split("/");
-		const fileExtension = fileExtensionArray[fileExtensionArray.length-1];
+  $: {
+    // called on props change
+    if (dataToCSV !== prevDataToCSV && dataToCSV.length !== 0) {
+      prevDataToCSV = dataToCSV;
+      const csvData = PapaParse.unparse(dataToCSV);
+      onUpload
+        ? onUpload(csvData)
+        : console.log(
+            'Remember to define an onUpload function as props. CSV Data:',
+            csvData
+          );
+    }
+  }
 
-		if (file.size > maxFileSize) {
-			console.log("Above the max file size threshold")
-			return;
-		}
-		if (fileExtension.includes('csv') && file.size < maxFileSize ) {
+  function uploadFile(event) {
+    event.preventDefault();
+    const file = uploader.files[0];
 
-			const csvData = PapaParse.parse(
-				file,
-				{
-					complete: (results) => {
-						onUpload ? onUpload(results.data) : console.log("Remember to define an onUpload function as props. Parsed CSV:", results.data);
-					}
-				}
-			);
-		} else if (allowedFileExtensions.includes(fileExtension)) {
-			onUpload ? onUpload(file) : console.log("Remember to define an onUpload function as props. Plain file:", file);
-		} else {
-			console.log("Not an allowed file type");
-		}
-	}
+    const fileExtensionArray = file.type.split('/');
+    const fileExtension = fileExtensionArray[fileExtensionArray.length - 1];
+
+    if (file.size > maxFileSize) {
+      console.log('Above the max file size threshold');
+      return;
+    }
+    if (fileExtension.includes('csv') && file.size < maxFileSize) {
+      const csvData = PapaParse.parse(file, {
+        complete: (results) => {
+          onUpload
+            ? onUpload(results.data)
+            : console.log(
+                'Remember to define an onUpload function as props. Parsed CSV:',
+                results.data
+              );
+        },
+      });
+    } else if (allowedFileExtensions.includes(fileExtension)) {
+      onUpload
+        ? onUpload(file)
+        : console.log(
+            'Remember to define an onUpload function as props. Plain file:',
+            file
+          );
+    } else {
+      const csvData = PapaParse.parse(file, {
+        complete: (results) => {
+          onUpload
+            ? onUpload(results.data)
+            : console.log(
+                'Remember to define an onUpload function as props. Parsed CSV:',
+                results.data
+              );
+        },
+      });
+    }
+  }
 </script>
 
-<input 
-	bind:this={uploader}
-	on:change={uploadFile} 
-	type="file" 
-/>
+<input bind:this={uploader} on:change={uploadFile} type="file" />
